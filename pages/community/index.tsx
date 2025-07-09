8d8151302501b0dfa35e8ac2fa462e79fad235fa
Rosuljon <ikramjanov97@mail.ru>
2025-07-09 18:22:34 +0900

    feat: integrate meta tags on document private file

---
+++ pages/community/index.tsx
@@ -0,0 +1,11 @@
+const CommunityPage = () => {
+  console.log("CommunityPage Loaded");
+  return (
+    <div>
+      <h1>Community Page</h1>
+      <p>Welcome to the community page!</p>
+    </div>
+  );
+};
+
+export default CommunityPage;
---
+++ pages/property/[propertyId]/index.tsx
@@ -0,0 +1,11 @@
+const PropertyDetailPage = () => {
+  console.log("PropertyDetailPage Loaded");
+  return (
+    <div>
+      <h1>Property Detail Page</h1>
+      <p>This is the detail page for a specific property.</p>
+    </div>
+  );
+};
+
+export default PropertyDetailPage;
