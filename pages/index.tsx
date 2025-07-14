11e3a2b8cd8f5a9688780c41a9451d03f1138090
Rosuljon <ikramjanov97@mail.ru>
2025-07-14 10:44:04 +0900

    feat: publish homepage screen components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,30 @@
-import withLayoutHome from "@/libs/components/layout/LayoutHome";
+/*******************************
+ * BY DEFAULT PAGE ROUTER NEXTJS RUNS AS A CLIENT SIDE BUT _DOCUMENT.TSX FILE IS SERVER SIDE RENDERING
+ *
+ * ********************** */
+
+import withLayoutMain from "@/libs/components/layout/LayoutHome";
 import { Box, Container, Stack } from "@mui/material";
-import { brown, green } from "@mui/material/colors";
 import { NextPage } from "next";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import TrendProperties from "@/libs/components/homePage/TrendProperties";
+import Advertisement from "@/libs/components/homePage/Advertisement";
+import PopularProperties from "@/libs/components/homePage/PopularProperties";
+import TopAgents from "@/libs/components/homePage/TopAgents";
+import TopProperties from "@/libs/components/homePage/TopProperties";
 
 const Home: NextPage = () => {
   return (
     <Stack className="home-page">
-      <Stack>
-        <Stack className="container">Trend Properties</Stack>
-      </Stack>
-      <Stack>
-        <Stack className="container">Popular Properties</Stack>
-      </Stack>
-      <Stack>
-        <Stack>
-          <Stack className="container">Advertisement</Stack>
-        </Stack>
-        <Stack className="container">Top Properties</Stack>
-      </Stack>
-      <Stack>
-        <Stack className="container">Top Agents</Stack>
-      </Stack>
+      <TrendProperties />
+      <PopularProperties />
+      <Advertisement />
+      <TopProperties />
+      <TopAgents />
     </Stack>
   );
 };
 
-export default withLayoutHome(Home);
+export default withLayoutMain(Home);
