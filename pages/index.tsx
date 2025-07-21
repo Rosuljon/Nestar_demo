c860468ce221c310586b3300413089999ee0f198
Rosuljon <ikramjanov97@mail.ru>
2025-07-21 11:42:47 +0900

    feat: develop apollo client connection

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,31 +10,31 @@ import "swiper/css/navigation";
 import TopProperties from "@/libs/components/homepage/TopProperties";
 import TopAgents from "@/libs/components/homepage/TopAgents";
 import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
-// import { useQuery } from "@apollo/client";
-// import { GET_PROPERTIES } from "@/apollo/user/query";
+import { useQuery } from "@apollo/client";
+import { GET_PROPERTIES } from "@/apollo/user/query";
 
 const Home: NextPage = () => {
   // DEVICE: MOBILE vs PC
   const device = useDeviceDetect();
 
-  // const {
-  //   loading: getPropertiesLoading,
-  //   data: getPropertiesData,
-  //   error: getPropertiesError,
-  //   refetch: getPropertiesRefetch,
-  // } = useQuery(GET_PROPERTIES, {
-  //   fetchPolicy: "network-only",
-  //   variables: {
-  //     input: {
-  //       page: 1,
-  //       limit: 5,
-  //       sort: "createdAt",
-  //       direction: "DESC",
-  //       search: {},
-  //     },
-  //   },
-  // });
-  // console.log("getPropertiesData:==>", getPropertiesData);
+  const {
+    loading: getPropertiesLoading,
+    data: getPropertiesData,
+    error: getPropertiesError,
+    refetch: getPropertiesRefetch,
+  } = useQuery(GET_PROPERTIES, {
+    fetchPolicy: "network-only",
+    variables: {
+      input: {
+        page: 1,
+        limit: 5,
+        sort: "createdAt",
+        direction: "DESC",
+        search: {},
+      },
+    },
+  });
+  console.log("getPropertiesData:==>", getPropertiesData);
 
   if (device === "mobile") {
     return <Stack>HOME PAGE MOBILE</Stack>;
